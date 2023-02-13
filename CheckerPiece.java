68f7d7094d1de0280710e8d30b823cb38f55b24a
Ale6408 <122243251+Ale6408@users.noreply.github.com>
2023-02-13 10:44:43 -0500

    Update CheckerPiece.java

--- CheckerPiece.java
+++ CheckerPiece.java
@@ -1,30 +1,46 @@
+/*
+  * @author     Alejandro Alonso Piza
+  * @id         alejnadro.alonso@betheluniversity.edu
+  * @course     CSIS 321:  Programming 3
+  * @assignment Reversi Project
+  * @related    Piece
+ */
 public class CheckerPiece extends Piece {
     public static final int WHITE = 1;
-    public static final int BLACK = 2
+    public static final int BLACK = 2;
     
-    public CheckerPiece(int inType, int inColor) {
-        super(inType);
+    //Constructor
+    //Creates a piece of the indicated color (1=white, 2=black)
+    public CheckerPiece(int color) {
+        super(color);
     }
     
+    //Mutator
+    //Changes the type of the piece that you are checking
     public void setType(int myType){
         type = myType;
     }
     
-    public getOpp(){
-        if (inType == WHITE){
-            return BLACK
-        }else if (inType == BLACK){
-            return WHITE
+    //Accessor
+    //Gives you the opposite of the piece you are checking
+    public int getOpp(int myType){
+        if (myType == WHITE){
+            return BLACK;
+        }else if (myType == BLACK){
+            return WHITE;
+        }else{
+            return -1;
         }
     }
     
+    //Gives the character depending on teh type
     public char toChar() {
-        if (type == WIHTE){
+        if (type == WHITE){
             return 'w';
         }else if (type == BLACK){
             return 'b';
         }else{
-            return 'blank';
+            return '-';
         }
     }
 }
