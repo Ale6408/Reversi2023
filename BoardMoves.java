ed97a03f894c64dfcd20194104daa3aeb7ca7dc8
Ale6408 <122243251+Ale6408@users.noreply.github.com>
2023-02-14 22:35:08 -0500

    Update BoardMoves.java

--- BoardMoves.java
+++ BoardMoves.java
@@ -6,7 +6,6 @@
   * @related    Board
  */
 public class BoardMoves extends Board{
-    //Field variables
     private CheckerPiece[][] pieces;
     private static final int UP = 1;
     private static final int DOWN = 2;
@@ -18,14 +17,14 @@ public class BoardMoves extends Board{
     private static final int DOWNLEFT = 8;
     
     //Constructor
-    //It builds the board with the help of the class Board.java
+    //It buildst the board with the help of the class Board.java
     //It sets the pieces in the middle to start the game
     //It sets who the first player is (white)
     public BoardMoves(){
         new Board();
         CheckerPiece white = new CheckerPiece(1);
         CheckerPiece black = new CheckerPiece(2);
-        
+    
         setPiece(white, 3, 3);
         setPiece(black, 3, 4);
         setPiece(white, 4, 4);
@@ -33,7 +32,7 @@ public class BoardMoves extends Board{
     }
     //Mutator
     //It puts a piece anywhere in the board (needs to be finished) 
-    public void makeMove(Piece piece, int row, int column){
+    public void makeMove(CheckerPiece piece, int row, int column){
         setPiece(piece, row, column);
     }
     
@@ -42,8 +41,12 @@ public class BoardMoves extends Board{
     //Then, it checks to see if the direction that it wants to go has the opposite piece
     public boolean isLegalMove(CheckerPiece piece, int row, int column, int dir){
         if(getPiece(row, column).getType() == Piece.BLANK){
+            if(row-1 < 0 || row+1 > 7 || column-1 < 0 || column+1 > 7){
+                return false;
+            }
             switch(dir){
                 case UP:
+                    
                     if(getPiece(row-1, column).getType() == piece.getOpp(piece.getType())){
                         return true;
                     }
@@ -82,14 +85,13 @@ public class BoardMoves extends Board{
     
     //Checks to seee if the piece has a move or not
     public boolean hasMove(CheckerPiece piece){
-        int i;
         for(int row = 0; row < size; row++){
-            i = 0;
             for(int column = 0; column < size; column++){
-                if(isLegalMove(piece, row, column, i) == true){
-                    return true;
+                for (int i = 1; i <= size; i++){
+                    if(isLegalMove(piece, row, column, i) == true){
+                        return true;
+                    }
                 }
-                i++;
             }
         }
         return false;
@@ -103,6 +105,41 @@ public class BoardMoves extends Board{
             return 1;
         }
         return -1;
+    }
+    
+    //Checks to see if the game is over or not
+    public boolean gameOver(CheckerPiece player1, CheckerPiece player2){
+        if(!hasMove(player1) && !hasMove(player2)){
+            return true;  
+        }
+        return false;
+    }
+    
+    //Gives the results of the game
+    public String results(){
+        int white = 0;
+        int black = 0;
+        String str = "";
+        
+        for(int row = 0; row < size; row++){
+            for(int column = 0; column < size; column++){
+                if(getPiece(row, column).getType() == CheckerPiece.WHITE){
+                    white++;
+                }else if(getPiece(row, column).getType() == CheckerPiece.BLACK){
+                    black++;
+                }
+            }
+        }
+        str += "White: " + white + '\n';
+        str += "Black: " + black + '\n';
+        if(white > black){
+            str += "White wins!";
+        }else if(white < black){
+            str += "Black wins!";
+        }else{
+            str += "It's a tie!";
+        }
+        return str;
     }
      
 }
